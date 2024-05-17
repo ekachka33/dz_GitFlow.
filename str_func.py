e0b7fa9faac3a48e19afc7e708c663ebf63d8eae
ekachka33 <fetisova.eka007@gmail.com>
2024-05-17 18:59:42 +0700

    docstring1

--- str_func.py
+++ str_func.py
@@ -2,7 +2,7 @@ def change_letters(input_string):
     """
     Принимает строку и возвращает ее в верхнем регистре.
 
-    :param input_string: Входная строка.
+   :param input_string: Входная строка.
     :return: Строка в верхнем регистре.
     """
     return input_string.upper()
